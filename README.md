e28c671d95d34bcdf01ed1b4b729a5685d435959
T.J. Koepp <epicsbi@yahoo.com>
2017-12-20 20:33:30 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Neighborhood-Map-Udacity
+# Project 5 - Front-End-Web-Developer Nanodegree
+## Neighborhood Map - Indianapolis Indiana
+
+## Details of the App
+This application lists a number of venues to visit around the Indianapolis Metropolitan Area. When a user clicks on a marker or the title of the venue in the list, details about the venue are listed in an info window including the Name of the location, the address, and some details about the venue. 
+
+Users can also use the search function to filter through the locations.
+
+## API's
+
+API's from Google Maps and Foursquare were used to power this app.
+
+## How to run
+To run this application download all of the files and open the index.html file. For this application to run properly you must be connected to the internet.
+
+To view the interactive map on github pages go to https://github.com/ITGeekTJ/Neighborhood-Map-Udacity
--- js/app2.js
+++ js/app2.js
@@ -2,48 +2,52 @@
 var locations = [
     {
         title : 'Indianapolis Motor Speedway',
+        address: '4790 W. 16th St. Indianapolis, IN 46222',
         locale: {lat : 39.78977746416131, lng : -86.23399395765476},
         venueID: '4ad4bff8f964a52011ea20e3'
     },
     {
         title : 'Lucas Oil Stadium',
+        address: '500 S. Capitol Ave. Indianapolis, IN 46225',
         locale: {lat : 39.759975771677055, lng : -86.16354426702111},
         venueID: '4b154fdcf964a5205faf23e3'
     },
     {
         title : 'Monument Circle',
+        address: '1 Monument Cir. Indianapolis, IN 46204',
         locale: {lat : 39.76838243284741, lng : -86.15805911401239},
         venueID: '4adc9080f964a520482d21e3'
     },
     {
         title : 'Indianapolis Zoo',
+        address: '1200 W. Washington St. Indianapolis, IN 46222',
         locale: {lat : 39.76762824223296, lng : -86.17995500564575},
         venueID: '58daa1558bbb0b01f18ec1fd'
     },
     {
         title : 'White River State Park',
+        address: '650 W. Washington St. Indianapolis, IN 46204',
         locale: {lat : 39.76757876221424, lng : -86.1701488494873},
         venueID: '4d123f40ffa36ea85898f7da'
     },
     {
         title : 'Indiana State Museum',
+        address: '500 W. Washington St. Indianapolis, IN 46204',
         locale: {lat : 39.76906093768051, lng : -86.16933360715025},
         venueID: '4ae08848f964a5200c8021e3'
     },
     {
         title : 'Indiana State Fairgrounds',
+        address: '1202 E. 38th St. Indianapolis, IN 46205',
         locale: {lat : 39.82758608941335, lng : -86.13514095010628},
         venueID: '4ad4bff9f964a5201cea20e3'
 	}
 ]
 
 Location = function(data) {
-    var self = this;
     this.title = data.title;
     this.lat = data.lat;
     this.lng = data.lng;
-    this.street = '';
-    this.city = '';
     this.marker = data.marker;
 };
 
@@ -106,12 +110,16 @@ var initMap = function() {
     for (var i = 0; i < locations.length; i++) {
         var title = locations[i].title;
         var position = locations[i].locale;
+        var address = locations[i].address;
+        var venueID = locations[i].venueID;
 
         // Create markers for locations and store in an array
         var marker = new google.maps.Marker({
             map: map,
             position: position,
             title: title,
+            address: address,
+            id: venueID,
             animation: google.maps.Animation.DROP,
         });
 
@@ -134,7 +142,9 @@ var initMap = function() {
         if (infowindow.marker != marker) {
             infowindow.marker = marker;
 
-            infowindow.setContent('<div>' + marker.title + '</div>');
+            infowindow.setContent('<div>' + marker.title + '</div>' +
+                '<div>' + marker.address + '</div>' +
+                '<div>' + 'Description: ' + marker.description + '</div>');
 
             infowindow.open(map, marker);
 
@@ -145,27 +155,26 @@ var initMap = function() {
 
     // Foursquare API
     var foursquareRequest = function(marker) {
+        var apiURL = 'https://api.foursquare.com/v2/venues/';
         var clientID = 'Y4OZ0UCZWBLDXA4CUSPKH2XQOMY1AEIKES4JGFZKSCP1BN4V';
         var clientSecret = 'TV40WIAUUMSRJGMQMJHRY3JXEDIDYSZUF1H0YWXWHPT5CUR3';
         var version = '20170413';
         var venueFoursquareID = marker.id;
-
-        var foursquareURL = 'https://api.foursquare.com/v2/venues/search?ll=' + venueFoursquareID + '?client_id=' + clientID + '&client_secret=' + clientSecret + '&v=' + version;
+        
+        var foursquareURL = apiURL + venueFoursquareID + '?client_id=' + clientID + '&client_secret=' + clientSecret + '&v=' + version;
 
         $.ajax({
             url: foursquareURL,
             success: function(data) {
 
-                marker.rating = data.response.venue.rating;
-                marker.hours = data.response.venue.hours;
                 marker.description = data.response.venue.description;
                 populateInfoWindow(marker, largeInfoWindow);
             },
 
             error: function(error) {
-                largeInfoWindow.setContent('<div>' + marker.title +
-                    '<p>' + marker.address + '</div>' +
-                    '<p>' + 'Rating: ' + 'FourSquare cannot be reached');
+                largeInfoWindow.setContent('<div>' + marker.title + '</div>' +
+                '<div>' + marker.address + '</div>' +
+                '<div>' + 'Description: ' + 'FourSquare cannot be reached' + '</div>');
                 largeInfoWindow.open(map, marker);
             }
         });
